63f3822d9b076312cd5fbe1e1b42b459302214af
marpio <marcinkowski@mailbox.org>
2017-08-26 23:32:37 +0200

    change encypt/decrypt to use chunks of data

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -5,7 +5,7 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/hex"
-	"errors"
+	"fmt"
 	"io"
 
 	"golang.org/x/crypto/nacl/secretbox"
@@ -21,6 +21,7 @@ func CalculateHash(r io.Reader) (string, error) {
 }
 
 func Encrypt(encryptionKey string, data []byte) ([]byte, error) {
+	var encrypted []byte
 	secretKeyBytes, err := hex.DecodeString(encryptionKey)
 	if err != nil {
 		return nil, err
@@ -29,16 +30,26 @@ func Encrypt(encryptionKey string, data []byte) ([]byte, error) {
 	var secretKey [32]byte
 	copy(secretKey[:], secretKeyBytes)
 
-	var nonce [24]byte
-	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
-		return nil, err
-	}
+	chunkSize := 64 * 1024
+	length := len(data)
+	for i := 0; i < len(data); i = i + chunkSize {
+		end := i + chunkSize
+		if end > length {
+			end = length
+		}
+		nonce, err := genNonce()
+		if err != nil {
+			return nil, err
+		}
+		encryptedChunk := secretbox.Seal(nonce[:], data[i:end], &nonce, &secretKey)
+		encrypted = append(encrypted, encryptedChunk...)
 
-	encrypted := secretbox.Seal(nonce[:], data, &nonce, &secretKey)
+	}
 	return encrypted, nil
 }
 
 func Decrypt(encryptionKey string, encryptedData []byte) ([]byte, error) {
+	var decrypted []byte
 	secretKeyBytes, err := hex.DecodeString(encryptionKey)
 	if err != nil {
 		return nil, err
@@ -46,12 +57,31 @@ func Decrypt(encryptionKey string, encryptedData []byte) ([]byte, error) {
 
 	var secretKey [32]byte
 	copy(secretKey[:], secretKeyBytes)
-	var decryptNonce [24]byte
-	copy(decryptNonce[:], encryptedData[:24])
-	decrypted, ok := secretbox.Open(nil, encryptedData[24:], &decryptNonce, &secretKey)
-	if !ok {
-		return nil, errors.New("Could not decrypt data")
-	}
 
+	chunkSize := 64*1024 + 24
+	length := len(encryptedData)
+	for i := 0; i < len(encryptedData); i = i + chunkSize {
+		end := i + chunkSize
+		if end > length {
+			end = length
+		}
+		encryptedDataChunk := encryptedData[i:chunkSize]
+		var decryptNonce [24]byte
+		copy(decryptNonce[:], encryptedDataChunk[:24])
+		decryptedChunk, ok := secretbox.Open(nil, encryptedDataChunk[24:], &decryptNonce, &secretKey)
+		if !ok {
+			return nil, fmt.Errorf("Could not decrypt data")
+		}
+		decrypted = append(decrypted, decryptedChunk...)
+
+	}
 	return decrypted, nil
+}
+
+func genNonce() ([24]byte, error) {
+	var nonce [24]byte
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
+		return [24]byte{}, err
+	}
+	return nonce, nil
 }
