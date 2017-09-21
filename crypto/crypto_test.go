44a6407be5f2439deb264bef11e71f30d3ebb209
marpio <marcinkowski@mailbox.org>
2017-09-21 21:30:02 +0200

    add tests to the crypto package

---
+++ crypto/crypto_test.go
@@ -0,0 +1,20 @@
+package crypto
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestEncryptDecrypt(t *testing.T) {
+	var b bytes.Buffer
+	encKey := "b567ef1d391e8a10d94100faa34b7d28fdab13e3f51f94b8"
+	data := []byte("test string")
+
+	Encrypt(&b, encKey, bytes.NewReader(data))
+	var b2 bytes.Buffer
+	Decrypt(&b2, encKey, &b)
+
+	if string(b2.Bytes()) != string(data) {
+		t.Error("Encrypt - Decrypt error")
+	}
+}
