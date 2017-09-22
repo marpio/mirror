2a104d6e1bed14eb1f2b3e1b63f946f543af9aa6
marpio <marcinkowski@mailbox.org>
2017-09-22 22:03:38 +0200

    add test

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+func TestEncrypt(t *testing.T) {
+	var b bytes.Buffer
+	encKey := "b567ef1d391e8a10d94100faa34b7d28fdab13e3f51f94b8"
+	data := []byte("test string")
+
+	Encrypt(&b, encKey, bytes.NewReader(data))
+
+	if string(b.Bytes()) == string(data) {
+		t.Error("Encrypt output should not be equal the input.")
+	}
+}
+
 func TestEncryptDecrypt(t *testing.T) {
 	var b bytes.Buffer
 	encKey := "b567ef1d391e8a10d94100faa34b7d28fdab13e3f51f94b8"
